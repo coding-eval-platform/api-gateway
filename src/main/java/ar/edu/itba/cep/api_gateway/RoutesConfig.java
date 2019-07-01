653d9d2fc830f84ef1ec8dfdbdba49176888fd01
Juan Marcos Bellini <juanmbellini@gmail.com>
2019-07-01 01:20:40 -0300

    Integrate with Spring Cloud Gateway and Eureka
    - Setup the application name as part of the Spring Cloud bootstrap configuration
    - Add Eureka client stuff
    - Add routing for both the playground and evaluations service, using Eureka stuff

---
+++ src/main/java/ar/edu/itba/cep/api_gateway/RoutesConfig.java
@@ -0,0 +1,42 @@
+package ar.edu.itba.cep.api_gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for the API gateway routes.
+ */
+@Configuration
+public class RoutesConfig {
+
+    /**
+     * Creates a bean of {@link RouteLocator} in order to setup the routes the API gateway will handle.
+     *
+     * @param builder The {@link RouteLocatorBuilder} used to create the {@link RouteLocator}.
+     * @return A bean of a {@link RouteLocator} configured to handle the API gateway's routes.
+     */
+    @Bean
+    public RouteLocator routes(final RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(
+                        "executor-service",
+                        r -> r
+                                .path("/execution-requests/**")
+                                .uri("lb://playground-service")
+                )
+                .route(
+                        "evaluations-service",
+                        r -> r
+                                .path("/exams/**")
+                                .or()
+                                .path("/exercises/**")
+                                .or()
+                                .path("/test-cases/**")
+                                .and()
+                                .uri("lb://evaluations-service")
+                )
+                .build();
+    }
+}
