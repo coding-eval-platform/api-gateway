e6c4a5ff4ef20254efca95a002b428ecccd924dd
Juan Marcos Bellini <juan.bellini@wolox.com.ar>
2019-08-03 11:37:20 -0300

    Improve thew JwtTokenDecoder
    - Make the body be typed

--- src/main/java/ar/edu/itba/cep/api_gateway/security/bearer/jwt/JwtTokenDecoder.java
+++ src/main/java/ar/edu/itba/cep/api_gateway/security/bearer/jwt/JwtTokenDecoder.java
@@ -3,6 +3,8 @@ package ar.edu.itba.cep.api_gateway.security.bearer.jwt;
 import ar.edu.itba.cep.api_gateway.security.bearer.BearerTokenAuthentication;
 import ar.edu.itba.cep.api_gateway.security.bearer.TokenDecoder;
 import io.jsonwebtoken.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
@@ -10,7 +12,10 @@ import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
 
 import java.security.PublicKey;
-import java.util.*;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static ar.edu.itba.cep.api_gateway.security.bearer.jwt.Constants.ROLES_CLAIM;
@@ -28,7 +33,7 @@ public class JwtTokenDecoder implements TokenDecoder {
     /**
      * A {@link JwtHandlerAdapter} used to handle the decoding process.
      */
-    private final JwtHandlerAdapter<Jws<Claims>> jwtHandlerAdapter;
+    private final CustomJwtHandlerAdapter jwtHandlerAdapter;
 
 
     /**
@@ -42,69 +47,182 @@ public class JwtTokenDecoder implements TokenDecoder {
         this.jwtHandlerAdapter = new CustomJwtHandlerAdapter();
     }
 
-
     @Override
     public Mono<BearerTokenAuthentication> decode(final String rawToken) {
         Assert.hasText(rawToken, "The token must not be null or empty");
-        return Mono.just(rawToken)
-                .map(token -> Jwts.parser().setSigningKey(publicKey).parse(token, jwtHandlerAdapter))
-                .map(Jws::getBody)
-                .map(claims -> {
-                    // Previous step validated the following values
-                    final var tokenId = UUID.fromString(claims.getId());
-                    final var username = claims.getSubject();
-                    @SuppressWarnings("unchecked") final var roles = (Set<String>) claims.get(ROLES_CLAIM, Set.class);
-
-                    return new BearerTokenAuthentication(tokenId, username, roles);
-                })
-                .onErrorResume(JwtException.class, ignored -> Mono.defer(Mono::empty))
-                ;
+        return Mono.defer(
+                () -> Mono.just(rawToken)
+                        .flatMap(this::parseToken)
+                        .map(Jws::getBody)
+                        .flatMap(JwtTokenDecoder::buildFromClaims)
+        );
     }
 
     /**
-     * Custom implementation of {@link JwtHandlerAdapter} that will validate the token.
+     * Parses the given {@code token}.
+     *
+     * @param token The token to be pared.
+     * @return An {@link Optional} containing the parsed {@link Jws} of {@link Claims} if the given {@code rawToken}
+     * could be parsed with no problems, or empty otherwise.
+     * @apiNote This method also verifies the signature of the token.
      */
-    private static class CustomJwtHandlerAdapter extends JwtHandlerAdapter<Jws<Claims>> {
+    private Mono<Jws<CustomBody>> parseToken(final String token) {
+        try {
+            final var jws = Jwts.parser().setSigningKey(publicKey).parse(token, jwtHandlerAdapter);
+            return Mono.justOrEmpty(jws);
+        } catch (final JwtException e) {
+            return Mono.empty();
+        }
+    }
+
+    /**
+     * Builds a {@link BearerTokenAuthentication} from the given {@code claims}.
+     *
+     * @param claims The {@link CustomBody} from where the data is taken.
+     * @return A {@link BearerTokenAuthentication} built with the data taken from the given {@code claims}.
+     */
+    private static Mono<BearerTokenAuthentication> buildFromClaims(final CustomBody claims) {
+        return Mono.just(
+                new BearerTokenAuthentication(
+                        claims.getId(),
+                        claims.getUsername(),
+                        claims.getRoles()
+                )
+        );
+    }
+
+
+    /**
+     * A custom {@link JwtHandlerAdapter} of {@link Jws} of {@link CustomBody}.
+     * Note that this {@link JwtHandlerAdapter} will validate the token.
+     */
+    private static final class CustomJwtHandlerAdapter extends JwtHandlerAdapter<Jws<CustomBody>> {
 
         @Override
-        public Jws<Claims> onClaimsJws(final Jws<Claims> jws) {
-            final var header = jws.getHeader();
-            final var claims = jws.getBody();
+        public Jws<CustomBody> onClaimsJws(final Jws<Claims> jws) {
+            final var body = new CustomBody(
+                    extractId(jws),
+                    extractUsername(jws),
+                    extractRoles(jws)
+            );
 
-            // Check jti is not missing
-            final var jtiString = claims.getId();
-            if (!StringUtils.hasText(jtiString)) {
-                throw new MissingClaimException(header, claims, "Missing \"jwt id\" claim");
-            }
-            try {
-                UUID.fromString(jtiString); // Throws an IllegalArgumentException if the String is not a valid UUID
-            } catch (final IllegalArgumentException e) {
-                throw new MalformedJwtException("The \"jwt id\" claim must be a valid UUID", e);
-            }
-
-            // Check roles is not missing
-            final var rolesObject = claims.get(ROLES_CLAIM, Collection.class);
-            if (rolesObject == null) {
-                throw new MissingClaimException(header, claims, "Missing \"roles\" claim");
-            }
-            // Check roles Collection contains only Strings (discard those that are not strings)
-            final var roles = ((Collection<?>) rolesObject).stream()
-                    .filter(role -> role instanceof String)
-                    .collect(Collectors.toSet());
-            claims.put(ROLES_CLAIM, roles); // Update the collection (which discarded non String values).
-
-            // Check issued at date is present and it is not a future date
-            final var issuedAt = Optional.ofNullable(claims.getIssuedAt())
-                    .orElseThrow(() -> new MissingClaimException(header, claims, "Missing \"issued at\" date"));
-            if (issuedAt.after(new Date())) {
-                throw new MalformedJwtException("The \"issued at\" date is a future date");
-            }
-            // Check expiration date is not missing
-            if (claims.getExpiration() == null) {
-                throw new MissingClaimException(header, claims, "Missing \"expiration\" date");
-            }
-
-            return jws;
+            return new CustomJws(jws.getHeader(), body, jws.getSignature());
         }
+
+        /**
+         * Extracts the token's id from the given {@code jws} in the form of {@link UUID}.
+         *
+         * @param jws The {@link Jws} from where the id will be taken.
+         * @return The token's id (in {@link UUID} form).
+         * @throws MissingClaimException If there is no id in the given {@code jws}.
+         * @throws MalformedJwtException If the given {@code jws}'s {@link Claims#ID} claim cannot be parsed
+         *                               into a {@link UUID} instance
+         */
+        private static UUID extractId(final Jws<Claims> jws) throws MissingClaimException, MalformedJwtException {
+            final var claims = jws.getBody();
+            final var id = Optional.ofNullable(claims.getId())
+                    .filter(StringUtils::hasLength)
+                    .orElseThrow(() -> new MissingClaimException(jws.getHeader(), claims, "Missing \"jti\" claim"));
+            try {
+                return UUID.fromString(id);
+            } catch (final IllegalArgumentException e) {
+                throw new MalformedJwtException("The \"jti\" claim must be a valid UUID", e);
+            }
+        }
+
+        /**
+         * Extracts the username from the given {@code jws}.
+         *
+         * @param jws The {@link Jws} from where the username will be taken.
+         * @return The username.
+         * @throws MissingClaimException If there is no username in the given {@code jws}
+         *                               (i.e no {@link Claims#SUBJECT} claim).
+         * @implNote This method checks the {@link Claims#SUBJECT} claim in order to get the username.
+         */
+        private static String extractUsername(final Jws<Claims> jws) throws MissingClaimException {
+            final var claims = jws.getBody();
+            return Optional.ofNullable(claims.getSubject())
+                    .filter(StringUtils::hasLength)
+                    .orElseThrow(() -> new MissingClaimException(jws.getHeader(), claims, "Missing \"sub\" claim"));
+        }
+
+        /**
+         * Extracts the roles from the given {@code jws}.
+         *
+         * @param jws The {@link Jws} from where the roles will be taken.
+         * @return The roles.
+         * @throws MissingClaimException If there is no roles in the given {@code jws}
+         *                               (i.e no {@link Constants#ROLES_CLAIM} claim).
+         * @implNote This method checks the {@link Constants#ROLES_CLAIM} claim in order to get the roles.
+         */
+        private static Set<String> extractRoles(final Jws<Claims> jws) throws MissingClaimException {
+            final var claims = jws.getBody();
+            return Optional.ofNullable(claims.get(ROLES_CLAIM, Collection.class))
+                    .map(collection -> ((Collection<?>) collection))
+                    .orElseThrow(() -> new MissingClaimException(jws.getHeader(), claims, "Missing \"roles\" claim"))
+                    .stream()
+                    .filter(role -> role instanceof String)
+                    .map(String.class::cast)
+                    .collect(Collectors.toSet());
+        }
+    }
+
+
+    /**
+     * A custom implementation of a {@link Jws} that uses a {@link CustomBody} instance as its body.
+     */
+    @AllArgsConstructor
+    private static final class CustomJws implements Jws<CustomBody> {
+
+        /**
+         * The {@link JwsHeader}.
+         */
+        private final JwsHeader header;
+        /**
+         * The {@link CustomBody}.
+         */
+        private final CustomBody body;
+        /**
+         * The signature.
+         */
+        private final String signature;
+
+
+        @Override
+        public JwsHeader getHeader() {
+            return header;
+        }
+
+        @Override
+        public CustomBody getBody() {
+            return body;
+        }
+
+        @Override
+        public String getSignature() {
+            return signature;
+        }
+    }
+
+
+    /**
+     * A custom jwt body that only includes the token's id, the username (i.e the subject), and the roles assigned in
+     * the token.
+     */
+    @Getter
+    @AllArgsConstructor
+    private static final class CustomBody {
+        /**
+         * The jwt id.
+         */
+        private final UUID id;
+        /**
+         * The username (i.e the subject).
+         */
+        private final String username;
+        /**
+         * The roles assigned in the token (this is a custom claim).
+         */
+        private final Set<String> roles;
     }
 }
